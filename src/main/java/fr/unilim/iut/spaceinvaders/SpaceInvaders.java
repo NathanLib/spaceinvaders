33fd56097dcf614f93faab2b852a3c93190759ba
Nathan Liboutet <nathan.lib19@gmail.com>
2018-05-06 11:16:57 +0200

    Création de deux méthodes extraites :thumbsup:

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -3,22 +3,39 @@ package fr.unilim.iut.spaceinvaders;
 public class SpaceInvaders {
 	int longueur;
 	int hauteur;
+	Vaisseau vaisseau;
 
 	public SpaceInvaders(int longueur, int hauteur) {
 		this.longueur = longueur;
 		this.hauteur = hauteur;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int i = 0; i < hauteur; i++) {
-			for (int j = 0; j < longueur; j++) {
-				espaceDeJeu.append('.');
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+					espaceDeJeu.append('V');
+				else
+					espaceDeJeu.append('.');
 			}
 			espaceDeJeu.append('\n');
 		}
 		return espaceDeJeu.toString();
+	}
+
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		this.vaisseau = new Vaisseau(x,y);
+	}
+	
+	//Extract method
+	private boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(x, y);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau!=null;
 	}
 
 }
