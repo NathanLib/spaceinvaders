3934449eb66da4a49475f1f7a8b786be570f8dbe
Nathan Liboutet <nathan.lib19@gmail.com>
2018-06-01 09:51:11 +0200

    création packages model

--- src/main/java/fr/unilim/iut/spaceinvaders/model/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/SpaceInvaders.java
@@ -1,4 +1,4 @@
-package fr.unilim.iut.spaceinvaders;
+package fr.unilim.iut.spaceinvaders.model;
 
 import fr.unilim.iut.spaceinvaders.moteurJeu.Commande;
 import fr.unilim.iut.spaceinvaders.moteurJeu.Jeu;
