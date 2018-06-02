2d681c17c3b3929a6b04fc9e7193cd8028cd85f2
Romain Duraysseix <dudu.romain.8@gmail.com>
2018-06-02 14:06:38 +0200

    Réglage Constante

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Constante.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Constante.java
@@ -2,11 +2,11 @@ package fr.unilim.iut.spaceinvaders.model;
 
 public class Constante {
 
-	public static final int ESPACEJEU_LONGUEUR = 750;
-	public static final int ESPACEJEU_HAUTEUR = 800;
+	public static final int ESPACEJEU_LONGUEUR = 400;
+	public static final int ESPACEJEU_HAUTEUR = 500;
 
-	public static final int VAISSEAU_LONGUEUR = 30;
-	public static final int VAISSEAU_HAUTEUR = 20;
+	public static final int VAISSEAU_LONGUEUR = 40;
+	public static final int VAISSEAU_HAUTEUR = 35;
 	public static final int VAISSEAU_VITESSE = 10;
 
 	public static final char MARQUE_FIN_LIGNE = '\n';
@@ -15,12 +15,12 @@ public class Constante {
 	public static final char MARQUE_MISSILE = 'M';
 	public static final char MARQUE_ENVAHISSEUR = 'E';
 	
-	public static final int MISSILE_VITESSE = 20;
-	public static final int MISSILE_HAUTEUR = 10;
-	public static final int MISSILE_LONGUEUR = 5;
+	public static final int MISSILE_VITESSE = 40;
+	public static final int MISSILE_HAUTEUR = 20;
+	public static final int MISSILE_LONGUEUR = 3;
 	
-	public static final int ENVAHISSEUR_VITESSE = 10;	
-	public static final int ENVAHISSEUR_LONGUEUR = 30;
-	public static final int ENVAHISSEUR_HAUTEUR = 30;
+	public static final int ENVAHISSEUR_VITESSE = 7;	
+	public static final int ENVAHISSEUR_LONGUEUR = 35;
+	public static final int ENVAHISSEUR_HAUTEUR = 25;
 }
 
